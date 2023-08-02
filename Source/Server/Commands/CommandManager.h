324f9e322fabd6c593fe14e2826b0ccfea236854
utf-4096 <utf16+github@riseup.net>
2023-08-02 14:14:04 +0200

    CommandManager.h: Refactor command definition

--- Source/Server/Commands/CommandManager.h
+++ Source/Server/Commands/CommandManager.h
@@ -5,34 +5,36 @@
 
 void cmd_generate_ban(server_t* server, command_args_t arguments, float time, ip_t ip, char* reason);
 
-void cmd_admin(void* p_server, command_args_t arguments);
-void cmd_ban_custom(void* p_server, command_args_t arguments);
-void cmd_ban_range(void* p_server, command_args_t arguments);
-void cmd_admin_mute(void* p_server, command_args_t arguments);
-void cmd_clin(void* p_server, command_args_t arguments);
-void cmd_help(void* p_server, command_args_t arguments);
-void cmd_intel(void* p_server, command_args_t arguments);
-void cmd_inv(void* p_server, command_args_t arguments);
-void cmd_kick(void* p_server, command_args_t arguments);
-void cmd_kill(void* p_server, command_args_t arguments);
-void cmd_login(void* p_server, command_args_t arguments);
-void cmd_logout(void* p_server, command_args_t arguments);
-void cmd_master(void* p_server, command_args_t arguments);
-void cmd_mute(void* p_server, command_args_t arguments);
-void cmd_pm(void* p_server, command_args_t arguments);
-void cmd_ratio(void* p_server, command_args_t arguments);
-void cmd_reset(void* p_server, command_args_t arguments);
-void cmd_say(void* p_server, command_args_t arguments);
-void cmd_server(void* p_server, command_args_t arguments);
-void cmd_toggle_build(void* p_server, command_args_t arguments);
-void cmd_toggle_kill(void* p_server, command_args_t arguments);
-void cmd_toggle_team_kill(void* p_server, command_args_t arguments);
-void cmd_tp(void* p_server, command_args_t arguments);
-void cmd_tpc(void* p_server, command_args_t arguments);
-void cmd_unban(void* p_server, command_args_t arguments);
-void cmd_unban_range(void* p_server, command_args_t arguments);
-void cmd_undo_ban(void* p_server, command_args_t arguments);
-void cmd_ups(void* p_server, command_args_t arguments);
-void cmd_shutdown(void* p_server, command_args_t arguments);
+#define DECLARE_COMMAND(name) void name(void* p_server, command_args_t arguments);
+
+DECLARE_COMMAND(cmd_admin)
+DECLARE_COMMAND(cmd_ban_custom)
+DECLARE_COMMAND(cmd_ban_range)
+DECLARE_COMMAND(cmd_admin_mute)
+DECLARE_COMMAND(cmd_clin)
+DECLARE_COMMAND(cmd_help)
+DECLARE_COMMAND(cmd_intel)
+DECLARE_COMMAND(cmd_inv)
+DECLARE_COMMAND(cmd_kick)
+DECLARE_COMMAND(cmd_kill)
+DECLARE_COMMAND(cmd_login)
+DECLARE_COMMAND(cmd_logout)
+DECLARE_COMMAND(cmd_master)
+DECLARE_COMMAND(cmd_mute)
+DECLARE_COMMAND(cmd_pm)
+DECLARE_COMMAND(cmd_ratio)
+DECLARE_COMMAND(cmd_reset)
+DECLARE_COMMAND(cmd_say)
+DECLARE_COMMAND(cmd_server)
+DECLARE_COMMAND(cmd_toggle_build)
+DECLARE_COMMAND(cmd_toggle_kill)
+DECLARE_COMMAND(cmd_toggle_team_kill)
+DECLARE_COMMAND(cmd_tp)
+DECLARE_COMMAND(cmd_tpc)
+DECLARE_COMMAND(cmd_unban)
+DECLARE_COMMAND(cmd_unban_range)
+DECLARE_COMMAND(cmd_undo_ban)
+DECLARE_COMMAND(cmd_ups)
+DECLARE_COMMAND(cmd_shutdown)
 
 #endif
